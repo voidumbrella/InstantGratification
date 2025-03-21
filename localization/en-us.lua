f6ec76e806aa65d0d55a261c8cbe38ceb1d9722e
voidumbrella <voidumbrella@gmail.com>
2025-03-21 00:45:16 -0400

    Initial draft

---
+++ localization/en-us.lua
@@ -0,0 +1,45 @@
+return {
+	descriptions = {
+		Back = {},
+		Blind = {},
+		Edition = {},
+		Enhanced = {},
+		Joker = {
+			j_banner = {
+				name = "Banner",
+				text = {
+					"{C:chips}+#1#{} Chips for",
+					"every {C:attention}#2#{} s remaining",
+				},
+			},
+		},
+		Other = {},
+		Planet = {},
+		Spectral = {},
+		Stake = {},
+		Tag = {},
+		Tarot = {},
+		Voucher = {},
+	},
+	misc = {
+		achievement_descriptions = {},
+		achievement_names = {},
+		blind_states = {},
+		challenge_names = {},
+		collabs = {},
+		dictionary = {},
+		high_scores = {},
+		labels = {},
+		poker_hand_descriptions = {},
+		poker_hands = {},
+		quips = {},
+		ranks = {},
+		suits_plural = {},
+		suits_singular = {},
+		tutorial = {},
+		v_dictionary = {
+			time_bonus = "Time bonus ($#1# for every #2# s left)",
+		},
+		v_text = {},
+	},
+}
